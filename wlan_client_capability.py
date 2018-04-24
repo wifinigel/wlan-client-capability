837c4aa24febb8318edb4d4aa7d54af2c76445ae
Nigel Bowden <wifinigel@gmail.com>
2018-04-24 07:10:10 +0100

    Added stream capabilites

--- wlan_client_capability.py
+++ wlan_client_capability.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python
 from __future__ import print_function, unicode_literals, division
 import sys
-from textwrap import wrap
+import textwrap
 from scapy.all import *
 
 #  assoc req frame tag list numbers
+
+# power information
+power_min_max = "33"
+
 # channels supported by client
 supported_channels = "36"
 
@@ -47,7 +51,7 @@ def analyze_frame(assoc_req_frame, silent_mode=False, required_client=''):
     if required_client:
     
         # we have specified a client we are interested in, but this isn't it
-        if required_client.lower() != frame_src_addr:
+        if (required_client != 'any') and (required_client.lower() != frame_src_addr):
             print("Assoc request detected, wrong client: " + dot11.addr2 + " - (req client = " + required_client + ")")
             return(False)
     
@@ -78,49 +82,97 @@ def analyze_frame(assoc_req_frame, silent_mode=False, required_client=''):
         dot11_elt = dot11_elt.payload
 
     # start report
+    print('\n')
     print('-' * 60)
     print("Client capabilites report - Client MAC: " + frame_src_addr)
     print('-' * 60)
     
     capability_dict = {}
+    
     # check if 11n supported
     if ht_capabilities in dot11_elt_dict.keys():
-        capability_dict['802.11n'] = 'Supported'        
-    else:
-        capability_dict['802.11n'] = 'Not reported'
+        capability_dict['802.11n'] = 'Supported'
         
-        # check if 11ac supported
-    if vht_capabilities in dot11_elt_dict.keys():
-        capability_dict['802.11ac'] = 'Supported'
+        spatial_streams = 0
+        
+        # mcs octets 1 - 4 indicate # streams supported (up to 4 streams only)
+        for mcs_octet in range(3, 7):
+        
+            mcs_octet_value = dot11_elt_dict[ht_capabilities][mcs_octet]
+        
+            if (mcs_octet_value & 255):
+                spatial_streams += 1
+        
+        capability_dict['802.11n'] = 'Supported (' + str(spatial_streams) + 'ss)'
     else:
-        capability_dict['802.11ac'] = 'Not reported'
+        capability_dict['802.11n'] = 'Not reported*'
+        
+    # check if 11ac supported
+    if vht_capabilities in dot11_elt_dict.keys():
+        
+        mcs_upper_octet = dot11_elt_dict[vht_capabilities][5]
+        mcs_lower_octet = dot11_elt_dict[vht_capabilities][4]
+        mcs_rx_map = (mcs_upper_octet * 256) + mcs_lower_octet
+        
+        # define the bit pair we need to look at
+        spatial_streams = 0
+        stream_mask = 3
 
+        # move through each bit pair & test for '10' (stream supported)
+        for mcs_bits in range(1,9):
+                    
+            if (mcs_rx_map & stream_mask) != stream_mask:
+            
+                # stream mask bits both '1' when mcs map range not supported
+                spatial_streams += 1
+            
+            # shift to next mcs range bit pair (stream)
+            stream_mask = stream_mask * 4
+        
+        capability_dict['802.11ac'] = 'Supported (' + str(spatial_streams) + 'ss)'
 
+    else:
+        capability_dict['802.11ac'] = 'Not reported*'
+        
     # check if 11k supported
     if rm_capabilities in dot11_elt_dict.keys():
         capability_dict['802.11k'] = 'Supported'
     else:
-        capability_dict['802.11k'] = 'Not reported'
+        capability_dict['802.11k'] = 'Not reported*'
 
     # check if 11r supported
     if ft_capabilities in dot11_elt_dict.keys():
         capability_dict['802.11r'] = 'Supported'
     else:
-        capability_dict['802.11r'] = 'Not reported'
+        capability_dict['802.11r'] = 'Not reported*'
 
     # check if 11v supported
     if ext_capabilities in dot11_elt_dict.keys():
-
-        # bit 4 of octet 3 in the extended capabilites field
-        octet3 = dot11_elt_dict[ext_capabilities][2]
-        bss_trans_support = int('00001000', 2)
-        
-        if octet3 & bss_trans_support:
-            capability_dict['802.11v'] = 'Supported'
-        else:
-            capability_dict['802.11v'] = 'Not reported'
     
-    # print out capabilities
+        ext_cap_list = dot11_elt_dict[ext_capabilities]
+        
+        capability_dict['802.11v'] = 'Not reported*'
+    
+        # check octet 3 exists
+        if 3 <= len(ext_cap_list):
+
+            # bit 4 of octet 3 in the extended capabilites field
+            octet3 = ext_cap_list[2]
+            bss_trans_support = int('00001000', 2)
+            
+            # 'And' octet 3 to test for bss transition support
+            if octet3 & bss_trans_support:
+                capability_dict['802.11v'] = 'Supported'
+    
+    # check if power capabilites supported
+    if power_min_max in dot11_elt_dict.keys():
+
+        # octet 3 of power capabilites
+        max_power = dot11_elt_dict[power_min_max][1]
+        
+        capability_dict['Max_Power'] = str(max_power) + " dBm"
+    
+    # print out capabilities (in nice format)
     for key in capability_dict.keys():
         print("{:<20} {:<20}".format(key, capability_dict[key]))
 
@@ -136,22 +188,23 @@ def analyze_frame(assoc_req_frame, silent_mode=False, required_client=''):
         for i in range(channel_range):
             channel_list.append(start_channel + (i * 4))
     
-    print("\nSupported Channel list:\n")
+    print("\nReported supported channel list:\n")
     channel_list_str = ', '.join(map(str, channel_list))
-    print(channel_list_str)
+    print(textwrap.fill(channel_list_str, 60))
+    
+    print("\n\n" + textwrap.fill("* Reported client capabilities are dependant on these features being available from the wireless network at time of client association", 60) + "\n\n")
     
     return True
 
-
 def PktHandler(frame):
-
+    
     required_client = sys.argv[3]
-
+    wrpcap('last_frame.pcap', [frame])
     # attempt to analyze frame
     if (analyze_frame(frame, True, required_client)):
     
         # we got an assocation request frame and analyzed it OK - dump & exit
-        wrpcap('last_frame.cap', [frame])
+        wrpcap('last_frame.pcap', [frame])
         exit()
     else:
         # frame incorrect type, lets try again...
@@ -160,7 +213,7 @@ def PktHandler(frame):
 def Usage():
     print("\n Usage:\n")
     print("    read_assoc_frame.py -f <filename>")
-    print("    read_assoc_frame.py -c <mon interface> <client mac>\n")
+    print("    read_assoc_frame.py -c <mon interface> < client_mac | any >\n")
     exit()
 
 #################################################
@@ -170,6 +223,7 @@ def main():
     if len(sys.argv) < 2:
         Usage()
 
+    # Analyze client capabilities from pcap file
     if sys.argv[1] == '-f':
         # file name we are going to analyze
         filename = sys.argv[2]
@@ -182,11 +236,14 @@ def main():
 
         # perform analysis
         analyze_frame(assoc_req_frame)
-        
+
+    # Analyze client capabilities of client capture association frame
     elif sys.argv[1] == '-c':
         # capture live
         mon_iface = sys.argv[2]
         client_mac = sys.argv[3]
+        
+        print("\n Listening for client association frames...\n")
         
         sniff(iface=mon_iface, prn=PktHandler)
     else:
